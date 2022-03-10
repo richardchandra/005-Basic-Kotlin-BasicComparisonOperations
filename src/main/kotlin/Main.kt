538d6dbe81392bbc86a00177103d0df8abb11444
unknown <richardchandra99@gmail.com>
2022-03-10 16:20:46 +0700

    Initial commit for kotlin basic comparison operators learning

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,24 @@
+fun main(args: Array<String>) {
+    //Comparison
+    //Only for boolean
+    val a = 10
+    val b = 10
+
+    var result:Boolean = a < b
+    println(result)
+
+    result = a > b
+    println(result)
+
+    result = a <= b
+    println(result)
+
+    result = a >= b
+    println(result)
+
+    result = a == b
+    println(result)
+
+    result = a != b
+    println(result)
+}
